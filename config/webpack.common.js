0a6da877294dcc30d7ebc62d7b6855b61cc655c9
Jacobo <jacoborodicio@gmail.com>
2021-12-20 00:56:26 +0100

    wip: setting config to production (changing path)

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -4,9 +4,10 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
     entry: "./src/index.js",
     output: {
-        path: path.resolve(__dirname, "../dist"),
+        path: path.resolve(__dirname, "dist"),
         filename: "[name].[contenthash].js",
-        publicPath: "auto"
+        publicPath: "auto",
+        clean: true
     },
     module: {
         rules: [
